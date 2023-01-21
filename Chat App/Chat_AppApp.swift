45a1f49cba69249f92d1ad023606ceac317c7508
Amish Tufail <amish.tufail2002@gmail.com>
2023-01-21 13:51:44 +0500

    Initial Commit

---
+++ Chat App/Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppApp.swift
+//  Chat App
+//
+//  Created by Amish Tufail on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
